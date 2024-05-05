bd004dd5bade4fbbb31d67e47386f2206514c00b
JarIsCoding <derpyderp1098@gmail.com>
2024-05-05 11:51:21 -0700

    additions to requirements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,6 +43,24 @@ export default function Home() {
             </ul>
           </nav>
 
+          <div className='pt-20 w-1/2'>
+            <div>
+              <p className='text-white font-bold'>
+                Email
+              </p>
+              <a href="Jaredlybusiness@gmail.com">
+                <p>
+                  Jaredlybusiness@gmail.com
+                </p>
+              </a>
+              <p className='text-white font-bold'>
+                Phone
+              </p>
+              <p>
+                (209) 598-0217
+              </p>
+            </div>
+          </div>
           <div className='flex justify-between w-[60px] pt-20'>
             <a href="https://github.com/JarIsCoding" target='_blank'>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-6 w-6" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
@@ -81,7 +99,16 @@ export default function Home() {
 
           <br />
 
-          <div id='certifs' className=''>
+          <div id='skills'>
+            <p className='text-[20px] text-white font-bold'>
+              Skills
+            </p>
+            <p>
+              Some of my skills include, HTML, CSS, Javascript, Unity, ReactJS, Typescript, Angular, NextJS, Tailwind, Figma, Vercel, Azure Webservices, Bootstrap, Flowbite, and Github Repo.
+            </p>
+          </div>
+
+          <div id='certifs' className='pt-8'>
             <p className='text-[20px] text-white font-bold'>
               My Certificates
             </p>
@@ -114,6 +141,12 @@ export default function Home() {
                 </div>
 
               </div>
+
+              <a href="https://publuu.com/flip-book/493281/1107445" className='pt-3 font-bold'>
+                  <div>
+                    More information in my <span className='text-white'>resume</span>
+                  </div>
+                </a>
             </div>
           </div>
 
